7724f50f16ec37b596e9b5c5e208a95235a9cb07
Jade Sabina <jsabina@mi-d3.msk.21-school.ru>
2020-09-07 18:58:54 +0300

    week3

--- Stage_1/week_2/decorator.py
+++ Stage_1/week_2/decorator.py
@@ -1,1 +1,10 @@
-print(type())
+import json
+import functools
+
+
+def to_json(func):
+    @functools.wraps(func)
+    def wrapped(*args, **kwargs):
+        result = func(*args, **kwargs)
+        return json.JSONEncoder().encode(result)
+    return wrapped
