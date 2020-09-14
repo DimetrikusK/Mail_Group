c86b4af700362075c60f63e6884e3e145a733308
Jade Sabina <jsabina@mi-d5.msk.21-school.ru>
2020-09-14 18:30:20 +0300

    done

--- Stage_1/week_3/solution.py
+++ Stage_1/week_3/solution.py
@@ -11,11 +11,12 @@ class CarBase:
         self.carrying = float(carrying)
 
     def get_photo_file_ext(self):
-        string = os.path.splitext(self.photo_file_name)
-        if isinstance(string, str):
-            print(string)
-        else:
-            print("ERROR")
+        try:
+            string = os.path.splitext(self.photo_file_name)
+            isinstance(string, str)
+            return string[1]
+        except ValueError:
+            return str
 
 
 class Car(CarBase):
@@ -36,22 +37,34 @@ class Truck(CarBase):
     def __init__(self, brand, photo_file_name, carrying, body_whl):
         super().__init__(brand, photo_file_name, carrying)
         self.body_whl = body_whl
-        tmp = body_whl.split('x')
-        try:
-            if float(tmp[0]) > 0 and float(tmp[1]) > 0 and float(tmp[2]) > 0 and len(tmp) == 3:
-                self.body_length = float(tmp[0])
-                self.body_width = float(tmp[1])
-                self.body_height = float(tmp[2])
-        except ValueError:
-            print("Невалидное значение")
+        if body_whl == 0:
+            self.body_length = float(0)
+            self.body_width = float(0)
+            self.body_height = float(0)
+        else:
+            try:
+                tmp = body_whl.split('x')
+                if float(tmp[0]) > 0 and float(tmp[1]) > 0 and float(tmp[2]) > 0 and len(tmp) == 3:
+                    self.body_length = float(tmp[0])
+                    self.body_width = float(tmp[1])
+                    self.body_height = float(tmp[2])
+            except ValueError:
+                self.body_length = float(0)
+                self.body_width = float(0)
+                self.body_height = float(0)
+
+    def get_body_volume(self):
+        return self.body_length * self.body_width * self.body_height
 
 
 class SpecMachine(CarBase):
     car_type = 'spec_machine'
-
-    def __init__(self, brand, photo_file_name, carrying, extra):
-        super().__init__(brand, photo_file_name, carrying)
-        self.extra = str(extra)
+    try:
+        def __init__(self, brand, photo_file_name, carrying, extra):
+            super().__init__(brand, photo_file_name, carrying)
+            self.extra = str(extra)
+    except:
+        list()
 
 
 def get_car_list(csv_filename):
@@ -63,20 +76,47 @@ def get_car_list(csv_filename):
         for row in reader:
             car_list.append([i for i in row if i != ''])
         for row in car_list:
-            if len(row) == 5:
-                if ('.jpg' in row[2]) or ('.jpeg' in row[2]) or ('.png' in row[2]) or ('.gif' in row[2]) or \
-                        ('.jpg' in row[3]) or ('.jpeg' in row[3]) or ('.png' in row[3]) or ('.gif' in row[3]):
-                    car_list_2.append(row)
+            if len(row) > 0:
+                if len(row) == 5 or row[0] == 'truck' and len(row) >= 4:
+                    if (row[2][-4:] == ".jpg" and len(row[2]) > 4) or (row[2][-5:] == ".jpeg" and len(row[2]) > 5) or (row[2][-4:] == '.png' and len(row[2]) > 4) \
+                            or (row[2][-4:] == '.gif' and len(row[2]) > 4) or (row[3][-4:] == '.jpg' and len(row[3]) > 4) or (row[3][-5:] == '.jpeg' and len(row[3]) > 5) \
+                            or (row[3][-4:] == '.png' and len(row[3]) > 4) or (row[3][-4:] == '.gif' and len(row[3]) > 4):
+                        car_list_2.append(row)
         car_list = []
+        print(car_list_2)
         for i in car_list_2:
-            if i[0] == 'truck':
-                car_list.append(Truck(i[1], i[2], i[3], i[4]))
+            if i[0] == 'truck' and len(i) == 5:
+                try:
+                    float(i[4])
+                    car_list.append(Truck(i[1], i[2], i[4], i[3]))
+                except:
+                    pass
+            elif i[0] == 'truck' and len(i) == 4:
+                try:
+                    float(i[3])
+                    car_list.append(Truck(i[1], i[2], i[3], 0))
+                except:
+                    pass
             elif i[0] == 'car':
-                car_list.append(Car(i[1], i[2], i[3], i[4]))
+                if i[2].isdigit():
+                    try:
+                        float(i[4])
+                        car_list.append(Car(i[1], i[3], i[4], i[2]))
+                    except:
+                        pass
             elif i[0] == 'spec_machine':
-                car_list.append(Car(i[1], i[2], i[3], i[4]))
+                try:
+                    float(i[3])
+                    car_list.append(SpecMachine(i[1], i[2], i[3], i[4]))
+                except:
+                    pass
         return car_list
 
 
-spec_machine = SpecMachine('Komatsu-D355', 'sad.png', '93', 'pipelayer specs')
-spec_machine.get_photo_file_ext()
+# #
+# car = get_car_list('/Users/jsabina/PycharmProjects/Mail_Group/Stage_1/week_3/cars.csv')
+# print(len(car))
+# spec_machine = SpecMachine('Komatsu-D355', 'd355', '93', 'pipelayer specs')
+# print(spec_machine.get_photo_file_ext())
+# car = Car('Bugatti Veyron', 'bugatti.png', '0.312', '2')
+# print(car.car_type, car.brand, car.photo_file_name, car.carrying, car.passenger_seats_count, sep='\n')
