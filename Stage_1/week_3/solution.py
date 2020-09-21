8f9b418993009b8d2ff0d455a3fc2116a2d861bb
Киселёв Дмитрий Андреевич <kiselyov.dmitrij@otr.ru>
2020-09-21 11:56:19 +0300

    multi file

--- Stage_1/week_3/solution.py
+++ Stage_1/week_3/solution.py
@@ -8,14 +8,14 @@ class CarBase:
     def __init__(self, brand, photo_file_name, carrying):
         self.brand = brand
         self.photo_file_name = photo_file_name
-        self.carrying = float(carrying)
+        self.carrying = carrying
 
     def get_photo_file_ext(self):
-        string = os.path.splitext(self.photo_file_name)
-        if isinstance(string, str):
-            print(string)
-        else:
-            print("ERROR")
+        try:
+            string = os.path.splitext(self.photo_file_name)
+            return string[1]
+        except TypeError:
+            return str
 
 
 class Car(CarBase):
@@ -23,7 +23,7 @@ class Car(CarBase):
 
     def __init__(self, brand, photo_file_name, carrying, passenger_seats_count):
         super().__init__(brand, photo_file_name, carrying)
-        self.passenger_seats_count = int(passenger_seats_count)
+        self.passenger_seats_count = passenger_seats_count
 
 
 class Truck(CarBase):
@@ -42,8 +42,11 @@ class Truck(CarBase):
                 self.body_length = float(tmp[0])
                 self.body_width = float(tmp[1])
                 self.body_height = float(tmp[2])
-        except ValueError:
+        except ValueError or IndexError:
             print("Невалидное значение")
+
+    def get_body_volume(self):
+        return self.body_length * self.body_width * self.body_height
 
 
 class SpecMachine(CarBase):
@@ -57,26 +60,30 @@ class SpecMachine(CarBase):
 def get_car_list(csv_filename):
     car_list = []
     car_list_2 = []
-    with open(csv_filename, encoding='utf-8') as csv_fd:
-        reader = csv.reader(csv_fd, delimiter=';')
-        next(reader)
-        for row in reader:
-            car_list.append([i for i in row if i != ''])
-        for row in car_list:
-            if len(row) == 5:
-                if ('.jpg' in row[2]) or ('.jpeg' in row[2]) or ('.png' in row[2]) or ('.gif' in row[2]) or \
-                        ('.jpg' in row[3]) or ('.jpeg' in row[3]) or ('.png' in row[3]) or ('.gif' in row[3]):
-                    car_list_2.append(row)
-        car_list = []
-        for i in car_list_2:
-            if i[0] == 'truck':
-                car_list.append(Truck(i[1], i[2], i[3], i[4]))
-            elif i[0] == 'car':
-                car_list.append(Car(i[1], i[2], i[3], i[4]))
-            elif i[0] == 'spec_machine':
-                car_list.append(Car(i[1], i[2], i[3], i[4]))
-        return car_list
+    try:
+       with open(csv_filename, encoding='utf-8') as csv_fd:
+            reader = csv.reader(csv_fd, delimiter=';')
+            next(reader)
+            for row in reader:
+                car_list.append([i for i in row if i != ''])
+            print(car_list)
+            for row in car_list:
+                if len(row) == 5:
+                    if ('.jpg' in row[2]) or ('.jpeg' in row[2]) or ('.png' in row[2]) or ('.gif' in row[2]) or \
+                            ('.jpg' in row[3]) or ('.jpeg' in row[3]) or ('.png' in row[3]) or ('.gif' in row[3]):
+                        car_list_2.append(row)
+            car_list = []
+            for i in car_list_2:
+                if i[0] == 'truck':
+                    car_list.append(Truck(i[1], i[2], i[4], i[1]))
+                elif i[0] == 'car':
+                    car_list.append(Car(i[1], i[2], i[4], i[3]))
+                elif i[0] == 'spec_machine':
+                    car_list.append(Car(i[1], i[2], i[3], i[4]))
+            return car_list
+    except ValueError or IndexError:
+        return list()
 
 
-spec_machine = SpecMachine('Komatsu-D355', 'sad.png', '93', 'pipelayer specs')
-spec_machine.get_photo_file_ext()
+cars = get_car_list('csv_filename.csv')
+print(len(cars))
---
+++ Stage_1/week_4/solution.py
@@ -0,0 +1,50 @@
+import os
+import tempfile
+
+
+class File:
+    direct = 'file'
+
+    def __init__(self, dir):
+        self.dir = dir
+        with open(self.dir, 'w') as f:
+            f.write('')
+
+    def __add__(self, other):
+        file = File(os.path.join(tempfile.gettempdir(), self.direct))
+        with open(file.dir, 'w') as f:
+            with open(self.dir, 'r') as j:
+                with open(other.dir, 'r') as n:
+                    f.write(j.read()), (f.write(n.read()))
+        return file
+
+    def __str__(self):
+        return self.dir
+
+    def read(self):
+        with open(self.dir, 'r') as f:
+            print(f.read())
+
+    def write(self, string):
+        with open(self.dir, 'w') as f:
+            f.write(string)
+            print(len(string))
+
+
+path_to_file = 'some_filename'
+# os.path.exists(path_to_file)
+# file_obj = File(path_to_file)
+# os.path.exists(path_to_file)
+# file_obj.read()
+# file_obj.write('some text')
+# file_obj.read()
+# file_obj.write('other text')
+# file_obj.read()
+file_obj_1 = File(path_to_file + '_1')
+file_obj_2 = File(path_to_file + '_2')
+file_obj_1.write('line 1\n')
+file_obj_2.write('line 2\n')
+new_file_obj = file_obj_1 + file_obj_2
+print(new_file_obj)
+print(isinstance(new_file_obj, File))
+print(type(new_file_obj))
