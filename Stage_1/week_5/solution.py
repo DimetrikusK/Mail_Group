f9ca5fb89c8138717c58005a43454f38ae1ff0ed
Jade Sabina <jsabina@mi-c5.msk.21-school.ru>
2020-10-01 18:34:13 +0300

    new_start

--- Stage_1/week_5/solution.py
+++ Stage_1/week_5/solution.py
@@ -2,11 +2,8 @@ import socket
 import time
 
 
-# sock = socket.create_connection(("127.0.0.1", 12223))
-# sock.sendall(b"ping")
-# sock.close()
-# class Error:
-#     pass
+class ClientError(Exception):
+    pass
 
 
 class Client:
@@ -14,19 +11,19 @@ class Client:
     def __init__(self, host, port, timeout=None):
         self.host = host
         self.port = port
-        self.timeout= timeout
-        self.sock = socket.create_connection((self.host, self.port), timeout)
-
+        self.timeout = (timeout or time.time())
+        try:
+            self.sock = socket.create_connection((self.host, self.port), self.timeout)
+            server = self.sock.recv(1024)
+            if b'ok\n' not in server:
+                ClientError(Exception)
+        except:
+            ClientError(Exception)
 
     def put(self, key, value, timestamp=None):
-        if not timestamp:
-            timestamp = str(int(time.time()))
+        timestamp = (timestamp or time.time())
         send = f'put {key} {value} {timestamp}\n'.encode('utf-8')
         self.sock.sendall(send)
-        server = self.sock.recv(1024)
-        if b'ok\n' not in server:
-            raise Exception
 
-    def get(self):
-        pass
-
+    def get(self, key):
+        dick_metric = {}
