81fd8ed9cc7bbe98a813613ff8bb71709ab5f5b8
Jade Sabina <jsabina@at-q7.msk.21-school.ru>
2020-10-03 16:18:05 +0300

    Shit

--- Stage_1/week_5/solution.py
+++ Stage_1/week_5/solution.py
@@ -11,19 +11,52 @@ class Client:
     def __init__(self, host, port, timeout=None):
         self.host = host
         self.port = port
-        self.timeout = (timeout or time.time())
+        self.timeout = (timeout or int(time.time()))
         try:
             self.sock = socket.create_connection((self.host, self.port), self.timeout)
-            server = self.sock.recv(1024)
-            if b'ok\n' not in server:
-                ClientError(Exception)
-        except:
-            ClientError(Exception)
+        except socket.error as err:
+            raise ClientError(err)
 
     def put(self, key, value, timestamp=None):
-        timestamp = (timestamp or time.time())
+        timestamp = (timestamp or int(time.time()))
         send = f'put {key} {value} {timestamp}\n'.encode('utf-8')
-        self.sock.sendall(send)
+        try:
+            self.sock.sendall(send)
+            server = self.sock.recv(1024)
+            if b'ok' not in server:
+                raise ClientError
+        except Exception:
+            raise ClientError
 
     def get(self, key):
         dick_metric = {}
+        send = f'get {key}\n'.encode('utf-8')
+        try:
+            self.sock.sendall(send)
+            server = self.sock.recv(1024)
+            if b'ok\n' not in server:
+                raise ClientError
+
+            server = str(server).strip('\n').split('\\n')
+
+            for m in server:
+                metrics = m.split(' ')
+                if len(metrics) == 3:
+                    metric_key = metrics[0]
+                    metric_value = float(metrics[1])
+                    metric_timestamp = int(metrics[2])
+                    metric_list = dick_metric.get(metric_key, [])
+                    metric_list.append((metric_timestamp, metric_value))
+                    dick_metric.update({metric_key: sorted(metric_list)})
+                elif metrics not in [[b'ok'], [""], [""]]:
+                    raise ClientError
+            return dick_metric
+        except socket.error as err:
+            raise ClientError(err)
+
+
+
+client = Client("127.0.0.1", 2222, timeout=15)
+# # client.put("eardrum.cpu", 4, timestamp=1150864251)
+# client.get("eardrum.memory")
+print(client.get("*"))
